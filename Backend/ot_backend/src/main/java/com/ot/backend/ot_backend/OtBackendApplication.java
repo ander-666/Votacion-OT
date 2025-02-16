65425d7afe724b2bbbbf147ad79b491c719f6d1e
Davomeist <svequiza@gmail.com>
2025-02-16 17:10:07 +0100

    first commit
    Co-authored-by: samu-1809 <samu-1809@users.noreply.github.com>
    
    Estructura de carpetas generada:
    Crear estructura del proyecto (backend) #8

---
+++ Backend/ot_backend/src/main/java/com/ot/backend/ot_backend/OtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ot.backend.ot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtBackendApplication.class, args);
+	}
+
+}
