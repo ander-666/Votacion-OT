38e837095a55635f3fef817f87103168d3e74ecc
Ander <ander.alonsom@gmail.com>
2025-03-19 12:40:28 +0100

    Cambio

--- Frontend/ot-votaciones/src/context/sessionProvider.jsx
+++ Frontend/ot-votaciones/src/context/sessionProvider.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useSession } from "../hooks/useSession";
-import { SessionContext } from "./sessionContext.js";
+import { useState } from "react";
+import { SessionContext } from "./sessionContext.js"
 
-export function SessionProvider({ children }) {
-  const { isLoggedIn, login, logout, user } = useSession();
-
-  return (
-    <SessionContext.Provider value={{ isLoggedIn, login, logout, user }}>
-      {children}
-    </SessionContext.Provider>
-  );
+export function SessionProvider ({ children }) {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return (
+        <SessionContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+           {children} 
+        </SessionContext.Provider>
+    )
 }
